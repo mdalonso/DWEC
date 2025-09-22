6c6b2a7e057209eb168393f76e4e29723d78051d
mdalonso <38704285+mdalonso@users.noreply.github.com>
2025-09-22 10:04:39 +0200

    Add files via upload

--- UD3/06-Document/js/app.js
+++ UD3/06-Document/js/app.js
@@ -1,19 +1,20 @@
 "use strict"
 
+
 // Usando document.write para mostrar un encabezado
-document.write("<h1>Información del Navegador y la Pantalla</h1>");
+document.body.innerHTML+="<h1>Información del Navegador y la Pantalla</h1>";
 
 // Usando document.writeln para escribir en una línea
-document.writeln("Este script utiliza document.write() y document.writeln().<br>");
+document.body.innerHTML+="Este script utiliza innerHTML para escribir en el documento.<br>";
 
 // Información del navegador
 
-document.write(`<strong>User Agent:</strong>   ${navigator.userAgent}<br>`);
-document.write(`<strong>Idioma:</strong>  ${navigator.language || navigator.userLanguage}<br>`);
-document.write(`<strong>Cookies Habilitadas:</strong> ${navigator.cookieEnabled ? "Sí" : "No"}<br>`);
+document.body.innerHTML+=`<strong>User Agent:</strong>   ${navigator.userAgent}<br>`;
+document.body.innerHTML+=`<strong>Idioma:</strong>  ${navigator.language || navigator.userLanguage}<br>`;
+document.body.innerHTML+=`<strong>Cookies Habilitadas:</strong> ${navigator.cookieEnabled ? "Sí" : "No"}<br>`;
 
 // Información de la pantalla
-document.writeln(`<strong>Ancho de la Pantalla:</strong> ${screen.width}  px<br>`);
-document.writeln(`<strong>Alto de la Pantalla:</strong> ${screen.height}  px<br>`);
-document.writeln(`<strong>Orientación:</strong>  ${screen.orientation ? screen.orientation.type : "No disponible"} <br>`);
-    
+document.body.innerHTML+=`<strong>Ancho de la Pantalla:</strong> ${screen.width}  px<br>`;
+document.body.innerHTML+=`<strong>Alto de la Pantalla:</strong> ${screen.height}  px<br>`;
+document.body.innerHTML+=`<strong>Orientación:</strong>  ${screen.orientation ? screen.orientation.type : "No disponible"} <br>`;
+/************************************************************** */
