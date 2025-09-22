59505494caaa60c57b34f00fe6ca7aa9cf599897
mdalonso <38704285+mdalonso@users.noreply.github.com>
2025-09-22 10:03:31 +0200

    Add files via upload

--- UD3/04-Navigator/js/app.js
+++ UD3/04-Navigator/js/app.js
@@ -1,24 +1,18 @@
 "use strict";
 
 // Propiedades del objeto navigator
-const userAgent = navigator.userAgent; // Información del navegador y sistema operativo
-const versionNavegador = navigator.userAgent; // Versión del navegador
+const userAgent = navigator.userAgent; // Información del navegador, motor de renderizado y sistema operativo
 const idioma = navigator.language || navigator.userLanguage; // Idioma del navegador
 const cookiesHabilitadas = navigator.cookieEnabled; // Si las cookies están habilitadas
 const online = navigator.onLine; // Estado de conexión a Internet
 
 
 //funciones
-function mostrarPosicion(posicion){
-    console.log(`Ubicación actual: Latitud ${posicion.coords.latitude}, Longitud ${posicion.coords.longitude}`);
-}
 
-function errorCor(err){
-    console.warn(`ERROR(${err.code}): ${err.message}`);
-}
+
 // Muestra la información en la consola
 console.log(`User Agent: ${userAgent}`);
-console.log(`Versión del Navegador: ${versionNavegador}`);
+//console.log(`Versión del Navegador: ${versionNavegador}`);
 console.log(`Idioma: ${idioma}`);
 console.log(`Cookies Habilitadas: ${cookiesHabilitadas ? "Sí" : "No"}`);
 console.log(`Estado de Conexión: ${online ? "Conectado" : "Desconectado"}`);
@@ -32,7 +26,6 @@ if (idioma==='es-ES'){
     console.log("El idioma del navegador no es el español");
 }
 
-
 //verificar si el navegador soporta geolocalización
 if (navigator.geolocation) {
     console.log("Este navegador soporta geolocalización.");
@@ -40,4 +33,12 @@ if (navigator.geolocation) {
        
 } else {
     console.log("Este navegador no soporta geolocalización.");
+}
+
+function mostrarPosicion(posicion){
+    console.log(`Ubicación actual: Latitud ${posicion.coords.latitude}, Longitud ${posicion.coords.longitude}`);
+}
+
+function errorCor(err){
+    console.warn(`ERROR(${err.code}): ${err.message}`);
 }
