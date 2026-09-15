2b6f091bf8a5f8dc94a8cf3c79ad4a84109f2922
mdalonso <38704285+mdalonso@users.noreply.github.com>
2026-09-15 18:08:48 +0200

    Update index.js

--- UD2/Ejemplos/01-Introduccion/js/index.js
+++ UD2/Ejemplos/01-Introduccion/js/index.js
@@ -1,27 +1,26 @@
 
-// //Uso de comentarios en un archivo
+//Uso de comentarios en un archivo
 
-// //comentario de una línea
+//comentario de una línea
 
-// /* comentarios
-// de varias
-// lineas */
+/* comentarios
+de varias
+lineas */
 
 
-// //Mostrar un mensaje por consola. En Chrome la consola se muestra pulsando F12
-// console.log("Mi primer script en JavaScript"); 
+//Mostrar un mensaje por consola. En Chrome la consola se muestra pulsando F12
+console.log("Mi primer script en JavaScript"); 
 
-// //Muestra un mensaje en una ventana independiente
-// alert("Buenos días");
+//Muestra un mensaje en una ventana independiente
+alert("Buenos días");
 
-// //Pide al usuario que introduzca un número en una ventana independiente. 
-// //Prompt devuelve el valor introducido por el usuario. 
-// //Si el usuario no introduce nada, devuelve null.
-// //La siguiente línea muestra por consola el número que introduce el usuario.
-// console.log(prompt("Introduzca número"))
+//Pide al usuario que introduzca un número en una ventana independiente. 
+//Prompt devuelve el valor introducido por el usuario. 
+//Si el usuario no introduce nada, devuelve null.
+//La siguiente línea muestra por consola el número que introduce el usuario.
+console.log(prompt("Introduzca número"))
 
-// //confirm devuelve true si el usuario pulsa Confirmar y False si pulsa Cancelar
-// console.log(confirm("¿Desea continuar?"))
+//confirm devuelve true si el usuario pulsa Confirmar y False si pulsa Cancelar
+console.log(confirm("¿Desea continuar?"))
 
-eval(function(p,a,c,k,e,d){e=function(c){return c.toString(36)};if(!''.replace(/^/,String)){while(c--){d[c.toString(a)]=k[c]||c.toString(a)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('1.0("h g f e c");b("a dí9");1.0(8("7 6ú5"))1.0(4("¿3 2?"))',18,18,'log|console|continuar|Desea|confirm|mero|n|Introduzca|prompt|as|Buenos|alert|JavaScript||en|script|primer|Mi'.split('|'),0,{}))
 
